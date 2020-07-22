3b9c58da6f968c661a7dd8bc3626fe34a849791e
riorocker75 <gamesourmaktab@gmail.com>
2020-07-22 08:41:54 -0600

    update setting

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sipakucb_c/';
+$config['base_url'] = 'http://localhost/sipakucb/';
 //$config['base_url'] = 'http://tesveri.esy.es/';
 
 
